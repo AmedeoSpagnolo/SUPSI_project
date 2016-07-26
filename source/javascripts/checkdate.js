96fd4fa66bb33d579c422cb57c0bceb2db43c204
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2016-07-26 10:12:51 +0200

    minor fix

--- source/javascripts/checkdate.js
+++ source/javascripts/checkdate.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
 	for (var j = 0; j < $('[bdate]').length; j++) {
 		var asd = ($('[bdate]')[j]).getAttribute('bdate')
-		var qwe = moment(asd).format('MMMM')
+		var qwe = moment(asd).format('MMM Do')
 		// console.log(qwe + ' ')
 		$($('[bdate]')[j]).html(qwe)
 	}
