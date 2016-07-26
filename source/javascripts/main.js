4a3ead8f27d30b0563aa313a3a0266e87f23fd0e
Gabriele Rossi <Gabriele Rossi>
2016-07-26 16:25:22 +0200

    adds map

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -2,8 +2,9 @@
 //= require jquery/dist/jquery
 //= require d3
 //= require moment
-//= require datamaps/dist/datamaps.world
 //= require topojson
+//= require datamaps/dist/datamaps.world
 
 //= require checkdate
 //= require birthinput
+//= require generate_map
