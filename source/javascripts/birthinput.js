6e1f9e73f14c56ce3ffec21112f36401dc740c9d
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2016-07-27 17:01:26 +0200

    modal opening function

--- source/javascripts/birthinput.js
+++ source/javascripts/birthinput.js
@@ -1,10 +1,29 @@
 $( document ).ready(function() {
 
-	// $(document)
-	// 	.on('click', '#sign', function(){
- //      $('#oroscope_content').html(horoscope[app.sign])
- //      $('#oroscope').css('opacity', 1)
-	// })
+	$(document)
+		.on('click', '#sign', function(){
+      openModal()
+
+  })
+
+    
+  $('#bg_hover').on('click', function(){
+    closeModal()
+  })
+
+  function openModal(){
+    $('.home').addClass('modal_open')
+    $('#modal').show().css({
+      'opacity': 1
+    })
+  }
+
+  function closeModal(){
+    $('.home').removeClass('modal_open')
+    $('#modal').css({
+      'opacity': 0
+    }).hide()
+  }
 
 });
 
