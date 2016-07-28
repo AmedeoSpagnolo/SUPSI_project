3b335e09911c310c552af23a7cd0a2c787e42733
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2016-07-28 10:06:00 +0200

    update date

---
+++ source/javascripts/leftinfo.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+
+  $('#info_date').html(moment().format('D.MM.YY'))
+});
--- source/javascripts/mouseposition.js
+++ source/javascripts/mouseposition.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
   d3.select('body').on('mousemove', function () {
     var c = d3.mouse(this),
         w = $(document).width(),
-        h = $(document).height();
-       mx = c[0] - w / 2
-       my = c[1] - h / 2
+        h = $(document).height(),
+       mx = (c[0] - w / 2),
+       my = (c[1] - h / 2);
+    $('#info_latitude').html(parseInt(mx,10))
+    $('#info_longitude').html(parseInt(my,10))
    })
 });
